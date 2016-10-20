feedc9c9d230b2799a5442d23e02a2c9fa6e09ae
潘家邦 <panjiabang@gmail.com>
2016-10-20 19:16:46 +0800

    add filter project_with_processing_tasks

--- omniweekly/filters/__init__.py
+++ omniweekly/filters/__init__.py
@@ -5,6 +5,7 @@ __all__ = [
     is_task_of,
     project_with_completed_tasks,
     project_with_incomplete_tasks,
+    project_with_processing_tasks,
     is_processing,
     is_complete,
     is_incomplete,
